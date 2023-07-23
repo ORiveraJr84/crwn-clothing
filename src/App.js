8837c5562624b95dc425632b700c80403421da42
ORiveraJr84 <oriverajr84@gmail.com>
2023-07-23 12:00:29 -0400

    started to build navigation. end of video 87.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component.jsx";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" />
+      </Route>
     </Routes>
   );
 };
